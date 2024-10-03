e52d916a6b3a663c187b6b001238ea14afd21e08
JackWu <qwertgb1234@gmail.com>
2024-10-03 18:43:24 +0800

    feat: add forex into finance summary
    Author: Aquila-Wu

--- yt_summarizer/models/finance_summary.py
+++ yt_summarizer/models/finance_summary.py
@@ -1,15 +1,20 @@
 from pydantic import BaseModel, Field
 
 from yt_summarizer.models.finance_template.etfs import ETF
-from yt_summarizer.models.finance_template.stock import stock
+from yt_summarizer.models.finance_template.forex import Forex
+from yt_summarizer.models.finance_template.stock import Stock
 
 
 class FinanceSummary(BaseModel):
-    stocks: list[stock] = Field(
+    stocks: list[Stock] = Field(
         [],
         description="A list of individual stocks. If no stocks are available, leave this list empty.",
     )
     etfs: list[ETF] = Field(
         [],
         description="A list of ETFs (Exchange-Traded Funds). If no ETFs are available, leave this list empty.",
+    )
+    forex: list[Forex] = Field(
+        [],
+        description="A list of forex pairs. If no forex pairs are available, leave this list empty.",
     )
--- yt_summarizer/models/finance_template/forex.py
+++ yt_summarizer/models/finance_template/forex.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
 
-class Trend(BaseModel):
+class Forex(BaseModel):
     name: str
     symbol: str
